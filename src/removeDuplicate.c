75095bbf27072b11acd4d9fcd80976523ac8f018
Andy <andy.li@b-padynamics.com>
2018-04-17 17:46:21 +0800

    Add automake for leetCode-C.

---
+++ src/removeDuplicate.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  removeDuplicate.c
+ *
+ *    Description:  给定一个排序数组，你需要在原地删除重复出现的元素，
+ *                  使得每个元素只出现一次，返回移除后数组的新长度
+ *
+ *        Version:  1.0
+ *        Created:  04/17/18 16:36:55
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Andy (https://github.com/andy-y-li), andy_y_li@163.com
+ *        Company:  gk
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int removeDuplicate(int *num, int numsSize) {
+
+}
+
+int main() {
+
+    return 0;
+}
