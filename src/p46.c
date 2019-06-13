8a6c149297bc30d236b57cdfe311fe2235c5abdb
Andy <andy.li@b-padynamics.com>
2019-06-13 17:34:00 +0800

    Add p46.

---
+++ src/p46.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  p46.c
+ *
+ *    Description: Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free(). 
+ *
+ *        Version:  1.0
+ *        Created:  04/26/19 22:10:28
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Andy (), andy_y_li@163.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+/*
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free(). 
+ *
+ */
+int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
+
+}
+
+int main()
+{
+
+    return 0;
+}
